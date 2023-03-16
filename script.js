5fe7adacf20d345350017984dbf996ab86c76a1a
Janet Webster <twixmixy@gmail.com>
2023-03-16 11:51:29 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var timeOfDay = [6, "noon", 8, "morning", "evening", 12];
+// var meals = [];
+// console.log(timeOfDay.length);
+// //6
+// console.log(timeOfDay);
+// //(6) [6, "noon", 8, "morning", "evening", 12]
+// // 0: 6
+// // 1: "noon"
+// // 2: 8
+// // 3: "morning"
+// // 4: "evening"
+// // 5: 12
+// console.log(timeOfDay[4]);
+// // evening
+// timeOfDay[4] = 5;
+// console.log(timeOfDay[4]);
+// //5
+// console.log(timeOfDay);
+// console.log(timeOfDay.includes("morning")); //true
+// console.log(timeOfDay.includes(11)); //false
+// timeOfDay.push("midnight");
+// console.log(timeOfDay);
+// timeOfDay.pop();
+// console.log(timeOfDay);
+// timeOfDay.splice(0, 2);
+// console.log(timeOfDay);
+// timeOfDay.splice(0, 2, "mid-afternoon", 7);
+// console.log(timeOfDay);
+// timeOfDay.splice(6, 0, "twilight", 5, 9);
+// console.log(timeOfDay);
+
+var employeeAge = [15, 36, 78, 17, 42, 19, 18];
+
+// var age = employeeAge.indexOf(42);
+// console.log(age);
+// // placement is 4
+var age = employeeAge.indexOf(37);
+console.log(age);
+// element does not exist, so it is -1
+
+//un-comment out anything above to test for yourself! <3 Twixmixy
